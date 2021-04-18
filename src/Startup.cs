307e1689b8b9c78d360a1788b70e853d396be3c5
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-04-18 13:47:16 +0200

    comply with Apache 2.0 license

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,3 +1,30 @@
+/*
+Copyright 2021 Microsoft Ireland Operations Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+I (Marc Lohrer) changed the file.
+
+This notice is intended to comply with the Apache Licence 2. 0 section 4.b. that states
+
+"4. You may reproduce and distribute copies of the Work or Derivative Works thereof in any medium, 
+ with or without modifications, and in Source or Object form, provided that You meet the following conditions:
+ ... 
+ b. You must cause any modified files to carry prominent notices stating that You changed the files; and
+ "
+*/
+
+
 using StiebelEltronApiServer.Models;
 using StiebelEltronApiServer.Repositories;
 using StiebelEltronApiServer.Services;
@@ -37,7 +64,6 @@ namespace StiebelEltronApiServer
             {
                 throw new Exception("Database connetion is not set.");
             }
-            // Add framework services.
 
             // DbContext pooling: AddDbContextPool enables pooling of DbContext instances. 
             // Context pooling can increase throughput in high-scale scenarios such as web servers by reusing context instances, 
