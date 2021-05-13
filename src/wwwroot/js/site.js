09160dc3cacada31c06368df06d3558cd84ee4e6
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-05-13 17:14:45 +0200

    align chart design

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿function drawChart (chartName, chartTitle, labels, maxYs, maxYsColors, averageYs, averageYsColor, minYs, minYsColor) {
+﻿;function drawMinMaxChart (chartName, chartTitle, labels, maxYs, maxYsColors, averageYs, averageYsColor, minYs, minYsColor) {
     var ctx = document.getElementById(chartName).getContext('2d');
     var data = {
         labels: labels,
@@ -12,14 +12,14 @@
         }, {
             label: "Mittel",
             borderWidth: 5,
-            data: minYs,
+            data: averageYs,
             fill: false,
             borderColor: 'rgba(140, 173, 88, 1)',
             backgroundColor: averageYsColor,
         }, {
             label: "Min",
             borderWidth: 5,
-            data: averageYs,
+            data: minYs,
             fill: false,
             borderColor: 'rgba(132, 133, 135, 1)',
             backgroundColor: minYsColor,
@@ -40,6 +40,74 @@
                 gridLines: {
                     display: true,
                     color: "rgba(rgba(176, 177, 177, 1))"
+                }
+            }],
+            xAxes: [{
+                type: 'time',
+                time: {
+                    parser: 'YYYY-MM-DD HH:mm:ss',
+                    unit: 'day'
+                },
+                ticks: {
+                    min: 0,
+                    beginAtZero: false
+                },
+                gridLines: {
+                    display: false
+                }
+            }]
+        }
+    };
+
+    new Chart(ctx, {
+        options: options,
+        data: data,
+        type: 'line'
+    });
+};
+
+;function drawStartEndChart (chartName, chartTitle, labels, startYs, startYsColors, deltaYs, deltaYsColor, endYs, endYsColor) {
+    var ctx = document.getElementById(chartName).getContext('2d');
+    var data = {
+        labels: labels,
+        datasets: [{
+            label: "Start",
+            borderWidth: 5,
+            data: startYs,
+            fill: false,
+            borderColor: 'rgba(233, 241, 221, 1)',
+            backgroundColor: startYsColors,
+        }, {
+            label: "Delta",
+            borderWidth: 5,
+            data: deltaYs,
+            fill: false,
+            borderColor: 'rgba(140, 173, 88, 1)',
+            backgroundColor: deltaYsColor,
+        }, {
+            label: "End",
+            borderWidth: 5,
+            data: endYs,
+            fill: false,
+            borderColor: 'rgba(132, 133, 135, 1)',
+            backgroundColor: endYsColor,
+        }]
+    };
+
+    var options = {
+        title: {
+            display: true,
+            text: chartTitle
+        },
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                },
+                gridLines: {
+                    display: true,
+                    color: "rgba(176, 177, 177, 1)"
                 }
             }],
             xAxes: [{
