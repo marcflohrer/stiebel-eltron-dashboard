0be45f0d3c13b2d3a83ab6347d80b305d94e063e
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-05-14 19:08:10 +0200

    remove unused using

--- src/Controllers/HeatPumpController.cs
+++ src/Controllers/HeatPumpController.cs
@@ -4,7 +4,6 @@ using StiebelEltronApiServer.Repositories;
 using System;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.Serialization;
 
 namespace StiebelEltronApiServer.Controllers
 {
