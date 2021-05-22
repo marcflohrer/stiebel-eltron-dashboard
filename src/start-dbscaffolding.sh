edad8d5fcdbce1e4caca212847fb032738c15e56
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-05-22 20:13:18 +0200

    fix warning

--- src/start-dbscaffolding.sh
+++ src/start-dbscaffolding.sh
@@ -4,7 +4,7 @@ mkdir -p mssql/data
 mkdir -p mssql/log
 mkdir -p mssql/secrets
 
-cd Data/Generated/ && find . ! -name . -prune ! -name ../Models -exec rm {} \; && cd ../..
+cd Data/Generated/ && find . ! -wholename . -prune ! -wholename ../Models -exec rm {} \; && cd ../..
 dotnet build
 dotnet user-secrets set ConnectionStrings:DefaultConnection "${DatabaseConnectionString}" --project .
 dotnet user-secrets set ServiceWeltUser "${ServiceWeltUser}" --project .
