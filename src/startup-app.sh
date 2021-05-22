9c9d928f8bc3709cffaf7bf9a5959395d0124321
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-05-22 19:14:34 +0200

    remove package update in Dockerfile

--- src/startup-app.sh
+++ src/startup-app.sh
@@ -6,7 +6,7 @@ mkdir -p mssql/log
 mkdir -p mssql/secrets
 
 dotnet build
-cd mssql/log/ && find . ! -name . -prune ! -name ../Models -exec rm {} \; && cd ../..
+cd mssql/log/ && find . ! -name . -prune ! -wholename ../Models -exec rm {} \; && cd ../..
 dotnet user-secrets init
 dotnet user-secrets set ConnectionStrings:DefaultConnection "${DatabaseConnectionString}" --project .
 dotnet user-secrets set ServiceWeltUser "${ServiceWeltUser}" --project .
@@ -14,5 +14,5 @@ dotnet user-secrets set ServiceWeltPassword "${ServiceWeltPassword}" --project .
 dotnet user-secrets set ServiceWeltUrl "${ServiceWeltUrl}" --project .
 docker-compose up --build --remove-orphans
 dotnet user-secrets clear
-cd mssql/log/ && find . ! -name . -prune ! -name ../Models -exec rm {} \; && cd ../..
+cd mssql/log/ && find . ! -name . -prune ! -wholename ../Models -exec rm {} \; && cd ../..
 #docker system prune -f
