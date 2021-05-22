5a41427b7d0e93464105c7888584860f6cf7dccb
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2021-05-22 20:13:46 +0200

    run process in background

--- src/startup-app.sh
+++ src/startup-app.sh
@@ -5,14 +5,14 @@ mkdir -p mssql/data
 mkdir -p mssql/log
 mkdir -p mssql/secrets
 
-dotnet build
+dotnet build &
 cd mssql/log/ && find . ! -name . -prune ! -wholename ../Models -exec rm {} \; && cd ../..
-dotnet user-secrets init
+dotnet user-secrets init 
 dotnet user-secrets set ConnectionStrings:DefaultConnection "${DatabaseConnectionString}" --project .
 dotnet user-secrets set ServiceWeltUser "${ServiceWeltUser}" --project .
 dotnet user-secrets set ServiceWeltPassword "${ServiceWeltPassword}" --project .
 dotnet user-secrets set ServiceWeltUrl "${ServiceWeltUrl}" --project .
-docker-compose up --build --remove-orphans
+docker-compose up --build --remove-orphans -d
 dotnet user-secrets clear
 cd mssql/log/ && find . ! -name . -prune ! -wholename ../Models -exec rm {} \; && cd ../..
 #docker system prune -f
